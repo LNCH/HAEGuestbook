4c951ffd920effab2cb1eccf23c60bef5449d20f
Tom Lynch [LIVE] <tom@lnch.co.uk>
2017-03-18 19:39:44 +0000

    Added main config file, holding DB connection info etc

---
+++ application/config/main.php
@@ -0,0 +1,20 @@
+<?php
+
+$config = [
+	
+	'title'	=> "HAE Group Guestbook",
+
+	'components' => [
+
+		'db' => [
+			'host' => 'db673988790.db.1and1.com',
+			'name' => 'db673988790',
+			'user' => 'dbo673988790',
+			'pass' => 'HAE2421!'
+		]
+
+	]
+
+];	
+
+return $config;
